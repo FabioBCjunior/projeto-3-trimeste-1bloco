9edd0ccbfb46cb0de81065c3e0063eb2fa341f90
FabioBCjunior <143199420+FabioBCjunior@users.noreply.github.com>
2024-09-27 09:05:22 -0300

    Add files via upload

--- Flashcards-main/perguntas.js
+++ Flashcards-main/perguntas.js
@@ -1,48 +1,119 @@
 criaCartao(
-    'Astronomia',
-    'O que é gravidade?',
-    'De acordo com a relatividade geral, é a deformação do espaço tempo causado por um objeto massivo como uma estrela ou um planeta, por exemplo'
+    'Biologia',
+    'Qual animal pode viver mais tempo sem comer?',
+    'A tartaruga gigante de Galápagos, que pode passar até 1 ano sem comer.'
 )
 
 criaCartao(
     'Biologia',
-    'O que é câncer?',
-    'É o crescimento desordenado das células devido a alterações no mecanismo celular'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da Austrália?',
-    'Camberra'
-)
-
-criaCartao(
-    'Inglês',
-    'Como se diz mar em inglês?',
-    'Sea'
+    'Qual animal tem o olfato mais aguçado?',
+    'O urso polar, que pode sentir o cheiro das presas a quilômetros de distância.'
 )
 
 criaCartao(
     'Matemática',
-    'Qual é a raiz quadrada de -4?',
-    'É mais ou menos 2i'
+    'Qual é a soma dos números de 1 a 10?',
+    '55.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o número que vem antes de 20 se contarmos para trás?',
+    '19.'
 )
 
 criaCartao(
     'Química',
-    'O que significa densidade?',
-    'É quanto determinada massa de um elemento ocupa certo espaço'
+    'Qual é a força que nos mantém presos no chão?',
+    'A força da gravidade.'
 )
 
 criaCartao(
-    'Física',
-    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
-    'Efeito fotoelétrico'
+    'Química',
+    'O que é matéria?',
+    'A matéria é tudo que tem massa e ocupa espaço.'
 )
 
 criaCartao(
-    'F',
-    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
-    'Efeito fotoelétrico'
+    'Geografia',
+    'Qual o maior continente do mundo?',
+    'Ásia.'
 )
 
+criaCartao(
+    'Geografia',
+    'Qual o Oceano mais profundo do mundo?',
+    'O Oceano Pacífico.'
+)
+
+criaCartao(
+    'História',
+    'Quem pintou a Monalisa?',
+    'Leonardo Da Vinci.'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu a América?',
+    'Cristóvão Colombo.'
+)
+
+criaCartao(
+    'Português',
+    'Qual o plural de pão?',
+    'Pães.'
+)
+
+criaCartao(
+    'Português',
+    'Qual o aumentativo de milho?',
+    'Milhão.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual a diferença entre ética e moral?',
+    'Ética é o etudo dos principios morais e a moral, são costumes e valores de uma sociedade.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual a diferença entre o idealismo e o materialismo?',
+    'Idealismo acredita que a realidade é mental ou espiritual, e o materialismo acredita que a realidade é material.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais proximo do Sol?',
+    'Mercúrio.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema Solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Artes',
+    'Qual é o nome da escultura mais famosa de Michelangelo?',
+    'Davi.'
+)
+
+criaCartao(
+    'Artes',
+    'Qual é o nome da dança clássica que se originou na Itália?',
+    'Balé.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é a clonagem?',
+    'É o processo de criar uma cópia genéticamente idêntica de organimo.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biocombustível?',
+    'São combutíveis produzidos a partir de matéria orgânica.'
+)
