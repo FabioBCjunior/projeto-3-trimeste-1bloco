66e462b3b98b9c20cf3cf6688b162f394156c06f
FabioBCjunior <143199420+FabioBCjunior@users.noreply.github.com>
2024-09-27 09:04:50 -0300

    Add files via upload

---
+++ projeto-3-trimeste-1bloco-main/Flashcards-main/perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Biologia',
+    'Qual animal pode viver mais tempo sem comer?',
+    'A tartaruga gigante de Galápagos, que pode passar até 1 ano sem comer.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual animal tem o olfato mais aguçado?',
+    'O urso polar, que pode sentir o cheiro das presas a quilômetros de distância.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a soma dos números de 1 a 10?',
+    '55.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o número que vem antes de 20 se contarmos para trás?',
+    '19.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a força que nos mantém presos no chão?',
+    'A força da gravidade.'
+)
+
+criaCartao(
+    'Química',
+    'O que é matéria?',
+    'A matéria é tudo que tem massa e ocupa espaço.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior continente do mundo?',
+    'Ásia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o Oceano mais profundo do mundo?',
+    'O Oceano Pacífico.'
+)
+
+criaCartao(
+    'História',
+    'Quem pintou a Monalisa?',
+    'Leonardo Da Vinci.'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu a América?',
+    'Cristóvão Colombo.'
+)
+
+criaCartao(
+    'Português',
+    'Qual o plural de pão?',
+    'Pães.'
+)
+
+criaCartao(
+    'Português',
+    'Qual o aumentativo de milho?',
+    'Milhão.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual a diferença entre ética e moral?',
+    'Ética é o etudo dos principios morais e a moral, são costumes e valores de uma sociedade.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual a diferença entre o idealismo e o materialismo?',
+    'Idealismo acredita que a realidade é mental ou espiritual, e o materialismo acredita que a realidade é material.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais proximo do Sol?',
+    'Mercúrio.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema Solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Artes',
+    'Qual é o nome da escultura mais famosa de Michelangelo?',
+    'Davi.'
+)
+
+criaCartao(
+    'Artes',
+    'Qual é o nome da dança clássica que se originou na Itália?',
+    'Balé.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é a clonagem?',
+    'É o processo de criar uma cópia genéticamente idêntica de organimo.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biocombustível?',
+    'São combutíveis produzidos a partir de matéria orgânica.'
+)
